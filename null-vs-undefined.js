7c4d58deaf5a82d36c9a337ce1cf9f854d5a0dc1
unknown <anwarhosen575@gmail.com>
2021-02-09 07:13:03 +0600

    null

---
+++ null-vs-undefined.js
@@ -0,0 +1,13 @@
+// let pakhi;
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+
+
+const result = add(13);
+console.log(result);

a23b5452b7899dcb3a40197ed6538f5c4ef4b9de
unknown <anwarhosen575@gmail.com>
2021-02-09 06:50:18 +0600

    change

---
+++ true-false.js
@@ -0,0 +1,18 @@
+  
+const age = 4;
+
+if(age > 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+const name = "salman";
+
+if(name.length){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}

0bced0180d29743178b0bcabffeb719d47cb4811
unknown <anwarhosen575@gmail.com>
2021-02-15 06:25:17 +0600

    Fixed

---
+++ array-spilt.js
@@ -0,0 +1,9 @@
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const part = nums.slice(2, 8);
+// console.log(part);
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const removed = nums.splice(2, 3);
+// console.log(removed);
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const part = nums.join(" , ");
+console.log(part);
